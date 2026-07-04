062781dce8ecf8f1679ca7e0fe507a54a6730c4a
ashmitham85-cloud <ashmitham85@gmail.com>
2026-07-04 11:27:25 +0530

    Add files via upload

---
+++ Deep Skilling/Module 1 - Design Patterns and Principles/SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("Application Started");
+
+            Console.WriteLine(ReferenceEquals(logger1, logger2));
+        }
+    }
+}
