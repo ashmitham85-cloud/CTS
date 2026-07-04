78b872c6125f2e73ecad47be7575edc1faadb426
ashmitham85-cloud <ashmitham85@gmail.com>
2026-07-04 11:42:56 +0530

    Add files via upload

---
+++ Deep Skilling/Module 2 - Data Structures and Algorithms/InventoryManagementSystem/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem
+{
+    public class Inventory
+    {
+        private Dictionary<int, Product> products = new Dictionary<int, Product>();
+
+        public void AddProduct(Product product)
+        {
+            products[product.ProductId] = product;
+            Console.WriteLine("Product added successfully.");
+        }
+
+        public void UpdateProduct(int id, int quantity, double price)
+        {
+            if (products.ContainsKey(id))
+            {
+                products[id].Quantity = quantity;
+                products[id].Price = price;
+                Console.WriteLine("Product updated successfully.");
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            if (products.Remove(id))
+            {
+                Console.WriteLine("Product deleted successfully.");
+            }
+        }
+
+        public void DisplayProducts()
+        {
+            Console.WriteLine("\nInventory:");
+
+            foreach (var product in products.Values)
+            {
+                Console.WriteLine(
+                    $"ID: {product.ProductId}, Name: {product.ProductName}, Quantity: {product.Quantity}, Price: {product.Price}");
+            }
+        }
+    }
+}
---
+++ Deep Skilling/Module 2 - Data Structures and Algorithms/InventoryManagementSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InventoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+
+            inventory.AddProduct(new Product(1, "Laptop", 10, 50000));
+            inventory.AddProduct(new Product(2, "Mouse", 50, 500));
+
+            inventory.DisplayProducts();
+
+            inventory.UpdateProduct(1, 15, 52000);
+
+            inventory.DisplayProducts();
+
+            inventory.DeleteProduct(2);
+
+            inventory.DisplayProducts();
+        }
+    }
+}
